83bf46877d24b3591a24cc99271a0553aff1153e
Ilya Afanasenko <34184692+felis2803@users.noreply.github.com>
2025-09-14 11:48:39 +0400

    refactor: type of tableMessageHandlers

--- src/cores/bitmex/tableMessageHandlers/index.ts
+++ src/cores/bitmex/tableMessageHandlers/index.ts
@@ -3,8 +3,8 @@ import { instrument } from './instrument';
 import type { BitMex } from '..';
 import type { BitMexChannel, BitMexChannelMessageAction, BitMexChannelMessageMap } from '../types';
 
-export const tableMessageHandlers: {
+export const tableMessageHandlers = { instrument } satisfies {
     [Channel in BitMexChannel]: {
         [Action in BitMexChannelMessageAction]: (core: BitMex, data: BitMexChannelMessageMap[Channel][]) => void;
     };
-} = { instrument };
+};
