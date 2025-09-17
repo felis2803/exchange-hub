909161fd8616a9188ccf0c041b093a39e2b12610
Ilya Afanasenko <34184692+felis2803@users.noreply.github.com>
2025-09-17 11:08:39 +0400

    chore: stub for index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-import type { ExchangeName } from './types';
-
-export { ExchangeHub } from './ExchangeHub';
-
 console.log('ExchangeHub initialized');
-
-declare module './cores/BaseCore' {
-    interface BaseCore<ExName extends ExchangeName> {
-        readonly instruments: unknown;
-    }
-}
